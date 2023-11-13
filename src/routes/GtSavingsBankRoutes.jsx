3e45bdf5a9cb0306bce37af69fe5268c02b7d782
julzedz <edoziechukwuma@gmail.com>
2023-11-13 23:19:38 +0100

    Setup Routes

--- src/routes/GtSavingsBankRoutes.jsx
+++ src/routes/GtSavingsBankRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Navigate, Route } from 'react-router';
 import CustomNavbar from '../components/navbar';
 
 const GtSavingsBankRoutes = () => (
@@ -6,7 +6,12 @@ const GtSavingsBankRoutes = () => (
     <CustomNavbar />
     <div className="tabs-container">
       <Routes>
-        <Route path="/home" />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/banking" element={<Banking />} />
+        <Route path="/investment" element={<Investment />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   </div>
