281dcea76840dbed5f1f97d0f3907ac94418a260
julzedz <edoziechukwuma@gmail.com>
2023-11-16 19:37:40 +0100

    Fix logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Box,
   Flex,
@@ -18,15 +18,7 @@ import logo from '../assets/bank-leaf.png';
 import '../styles/navbar.css';
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(null);
-
-  const handleDropdownOpen = (index) => {
-    setIsDropdownOpen(index);
-  };
-
-  const handleDropdownClose = () => {
-    setIsDropdownOpen(null);
-  };
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
     <Flex
@@ -39,6 +31,7 @@ const Navbar = () => {
       {/* Logo */}
       <Box>
         <Flex align="center" className="logo-group">
+
           <Image src={logo} alt="Logo" boxSize="50px" />
           <div>
             <Text
@@ -47,9 +40,13 @@ const Navbar = () => {
               color="white"
               fontFamily="Atomic Age"
               align="center"
+              my={0}
             >
               GT Savings Bank
-              <Text color="#97a722" style={{ display: 'inline' }}>
+              <Text
+                color="#97a722"
+                style={{ display: 'inline' }}
+              >
                 .
               </Text>
             </Text>
@@ -60,89 +57,79 @@ const Navbar = () => {
       {/* Nav Links */}
       <Flex align="center">
         <NavLink href="/">Home</NavLink>
-        <DropdownMenu label="Banking & Borrowing" index={1}>
+        <DropdownMenu label="Banking & Borrowing">
           <DropdownItem href="/banking">Online Banking</DropdownItem>
           <DropdownItem href="/loans">Loans</DropdownItem>
           <DropdownItem href="/mortgages">Mortgages</DropdownItem>
         </DropdownMenu>
-        <DropdownMenu label="Support" index={2}>
+        <DropdownMenu label="Support">
           <DropdownItem href="/support">Contact Us</DropdownItem>
           <DropdownItem href="/about">About Us</DropdownItem>
           <DropdownItem href="/about">Make an Appointment</DropdownItem>
         </DropdownMenu>
-        <DropdownMenu label="Investment Services" index={3}>
+        <DropdownMenu label="Investment Services">
           <DropdownItem href="/investment">Investment</DropdownItem>
           <DropdownItem href="/insurance">Insurance</DropdownItem>
         </DropdownMenu>
       </Flex>
     </Flex>
   );
+};
 
-  function NavLink({ children, href }) {
-    return (
-      <Link
-        href={href}
+const NavLink = ({ children, href }) => (
+  <Link
+    href={href}
+    fontSize="md"
+    color="white"
+    px={4}
+    py={2}
+    _hover={{ bg: 'gunmetal', color: 'applegreen' }}
+    borderRadius="md"
+    transition="background 0.4s"
+  >
+    {children}
+  </Link>
+);
+
+const DropdownMenu = ({ label, children }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Menu isOpen={isOpen} onToggle={onToggle}>
+      <MenuButton
+        as={Link}
         fontSize="md"
         color="white"
+        bg="black"
         px={4}
         py={2}
         _hover={{ bg: 'gunmetal', color: 'applegreen' }}
         borderRadius="md"
         transition="background 0.4s"
+        onMouseEnter={() => onToggle(true)}
+        onMouseLeave={() => onToggle(false)}
       >
-        {children}
-      </Link>
-    );
-  }
-
-  function DropdownMenu({ label, children, index }) {
-    return (
-      <Box
-        onMouseEnter={() => handleDropdownOpen(index)}
-        onMouseLeave={handleDropdownClose}
-      >
-        <Link
-          fontSize="md"
-          color="white"
-          bg="black"
-          px={4}
-          py={2}
-          _hover={{ bg: 'gunmetal', color: 'applegreen' }}
-          borderRadius="md"
-          transition="background 0.4s"
-        >
-          <Flex align="center">
-            <Text my={0}>{label}</Text>
-            <ChevronDownIcon />
-          </Flex>
-        </Link>
-        <Collapse in={isDropdownOpen === index}>
-          <MenuList
-            bg="gunmetal"
-            color="white"
-            borderRadius="lg"
-            _hover={{ bg: 'gunmetal', color: 'applegreen' }}
-          >
-            {children}
-          </MenuList>
-        </Collapse>
-      </Box>
-    );
-  }
-
-  function DropdownItem({ href, children }) {
-    return (
-      <MenuItem
-        as={Link}
-        href={href}
+        <Flex align="center">
+          <Text my={0}>{label}</Text>
+          <ChevronDownIcon />
+        </Flex>
+      </MenuButton>
+      <MenuList
         bg="gunmetal"
-        fontSize="md"
-        _hover={{ border: '0px' }}
+        color="white"
+        borderRadius="lg"
+        _hover={{ bg: 'gunmetal', color: 'applegreen' }}
       >
         {children}
-      </MenuItem>
-    );
-  }
+      </MenuList>
+    </Menu>
+  );
 };
+
+const DropdownItem = ({ href, children }) => (
+  <MenuItem as={Link} href={href} bg="gunmetal" fontSize="md" _hover={{ border: '0px' }}>
+    {children}
+  </MenuItem>
+);
 
 export default Navbar;
