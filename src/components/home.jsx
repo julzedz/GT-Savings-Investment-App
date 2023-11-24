92f517a6f6663b48e40173e0a78b0d1c32a6bd07
julzedz <edoziechukwuma@gmail.com>
2023-11-24 18:00:24 +0100

    Setup cover image with overlay

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,28 @@
+import React from 'react';
+import { Box, Image } from '@chakra-ui/react';
+import image from '../assets/skyscraper2.jpg';
+
 const Home = () => (
   <>
-    <h2>Dashboard</h2>
+    <Box
+      position="relative"
+      maxW="100%"
+    >
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        w="100%"
+        h="100%"
+        bgGradient="linear(to-l, #0003, #000)"
+      />
+      <Image
+        src={image}
+        alt=""
+        width="100%"
+      />
+    </Box>
+    <h1>Dashboard</h1>
   </>
 );
 
