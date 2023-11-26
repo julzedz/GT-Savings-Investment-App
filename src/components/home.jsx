b05acc91327bc7d95e8c8c4b9bf3f801e24bbd3f
julzedz <edoziechukwuma@gmail.com>
2023-11-26 19:39:39 +0100

    Add Get Started button

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,10 @@ import {
   Text,
   Heading,
   Image,
+  Button,
+  Flex,
 } from '@chakra-ui/react';
+import { ArrowDownIcon } from '@chakra-ui/icons';
 import coverImage from '../assets/skyscraper2.jpg';
 
 const Home = () => (
@@ -31,6 +34,7 @@ const Home = () => (
       <Box
         m={3}
         p={3}
+        pb={28}
         textColor="white"
         pos="absolute"
         top={{ base: '15', lg: '28' }}
@@ -39,14 +43,50 @@ const Home = () => (
         <Heading
           m={3}
           p={4}
-          textAlign={{ base: 'center', lg: 'auto' }}
+          textAlign={{ base: 'center', lg: 'left' }}
           fontSize={{ base: '5xl', lg: '7xl' }}
           fontWeight={{ base: 'bold', lg: 'black' }}
+          width="80%"
         >
           Build Wealth On Your Terms
         </Heading>
-        <Text>We are committed to helping you set and clarify your financial goals.</Text>
+        <Text
+          m={3}
+          p={4}
+          textAlign={{ base: 'center', lg: 'left' }}
+          fontSize={{ base: '2xl', lg: '3xl' }}
+          fontWeight={{ base: 'bold', lg: 'black' }}
+          letterSpacing="wide"
+        >
+          We are committed to helping you set and clarify your financial goals.
+        </Text>
       </Box>
+      <Flex
+        m={3}
+        p={3}
+        textColor="white"
+        pos="absolute"
+        top={{ base: '96', lg: '28' }}
+        left={{ base: '6', lg: '6' }}
+      >
+        <Button
+          mx={2}
+          bg="transparent"
+          variant="outline"
+          borderRadius="50%"
+          height="50px"
+          width="50px"
+          _hover={{ bg: 'gunmetal' }}
+        >
+          <ArrowDownIcon color="white" _hover={{ color: 'gunmetal' }} />
+        </Button>
+        <Text
+          mx={2}
+          my={3}
+        >
+          Let&apos;s Get Started
+        </Text>
+      </Flex>
     </Box>
     <h1>Dashboard</h1>
     <h1>Dashboard</h1>
