0886af9495a2b0713e5a14e56722e1d9c976a1f4
julzedz <edoziechukwuma@gmail.com>
2023-11-29 23:28:58 +0100

    Add grid to 2nd home component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,6 +5,8 @@ import {
   Heading,
   Image,
   Button,
+  Grid,
+  GridItem,
   Flex,
 } from '@chakra-ui/react';
 import { ArrowDownIcon } from '@chakra-ui/icons';
@@ -57,6 +59,7 @@ const Home = () => (
           fontSize={{ base: '2xl', lg: '3xl' }}
           fontWeight={{ base: 'bold', lg: 'black' }}
           letterSpacing="wide"
+          // color="applegreen"
         >
           We are committed to helping you set and clarify your financial goals.
         </Text>
@@ -66,9 +69,9 @@ const Home = () => (
           p={3}
           textColor="white"
           transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-          justifyContent="center"
+          justifyContent={{ base: 'center', lg: 'center' }}
           alignItems="center"
-          width={{ base: '100%', lg: 'auto%' }}
+          width={{ base: '100%', lg: 'auto' }}
         >
           <Button
             mx={2}
@@ -95,9 +98,59 @@ const Home = () => (
     <Box
       bgImage={bgsvg}
       w="100%"
-      height="auto"
+      height="7xl"
     >
-      <h1>Hope</h1>
+      <Grid
+        h="100%"
+        w="100%"
+        p={{ base: 8, lg: 12 }}
+        templateRows={{ base: 'repeat(3, 1fr)', lg: 'repeat(2, 1fr)' }}
+        templateColumns={{ base: '1fr', md: 'repeat(4, 1fr)' }}
+        gap={10}
+      >
+        <GridItem
+          colSpan={2}
+          bg="papayawhip"
+          borderRadius={21}
+          w="100%"
+          p={12}
+        >
+          <Box>
+            Hello World
+          </Box>
+          <Box>
+            Hello World
+          </Box>
+          <Box>
+            <h3>TAKE YOUR SAVINGS TO THE NEXT LEVEL</h3>
+            <Text>Now offering higher rate with minimum of $25,000 for Customers.</Text>
+            <Button
+              mx={2}
+              bg="gunmetal"
+              borderRadius="20px"
+              height="auto"
+              width="auto"
+              color="white"
+              p={3}
+              _hover={{ bg: 'applegreen', color: 'black' }}
+            >
+              OPEN ACCOUNT
+            </Button>
+          </Box>
+        </GridItem>
+        <GridItem
+          colSpan={2}
+          bg="papayawhip"
+          borderRadius={21}
+          w="100%"
+        />
+        <GridItem
+          colSpan={{ base: '2', md: '4' }}
+          rowSpan={4}
+          bg="tomato"
+          borderRadius={21}
+        />
+      </Grid>
     </Box>
     <h1>Dashboard</h1>
     <h1>Dashboard</h1>
