8c4194a2cb530c2a5117c1a7ffddf4ea7bcf5d81
julzedz <edoziechukwuma@gmail.com>
2023-12-07 23:22:28 +0100

    Add background picture to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,39 @@
+/* eslint-disable max-len */
+import { Box, Text, Button } from '@chakra-ui/react';
+import skyscraper from '../assets/skyscraper1.jpg';
+
 const Footer = () => (
   <>
-    <h1>My Footer</h1>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      backgroundImage={skyscraper}
+      bgSize="cover"
+
+    >
+      <Text
+        fontSize="sm"
+        fontFamily="lit"
+        fontWeight="thin"
+        letterSpacing="widest"
+        lineHeight="short"
+        textAlign="center"
+        mt={20}
+      >
+        GET IN TOUCH
+      </Text>
+      <Text>
+        We&apos;d love to speak with you.
+      </Text>
+      <Text>
+        Please call us at 800-947-6438, email us at info@gtsavingsbank.net or click the button below to access our contact form.
+      </Text>
+      <Button>
+        Contact Us
+      </Button>
+    </Box>
   </>
 );
 
