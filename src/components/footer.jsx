c42198314168f8500f1af09fd1b7a023e60b2230
julzedz <edoziechukwuma@gmail.com>
2023-12-08 13:12:21 +0100

    Add gradient to background.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,36 +1,45 @@
 /* eslint-disable max-len */
 import { Box, Text, Button } from '@chakra-ui/react';
-import skyscraper from '../assets/skyscraper1.jpg';
+import skyscraper from '../assets/blackandwhite.jpg';
 
 const Footer = () => (
   <>
     <Box
+      p={8}
+      w="100%"
       display="flex"
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      backgroundImage={skyscraper}
+      backgroundImage={`linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${skyscraper})`}
       bgSize="cover"
-
+      minHeight={32}
+      bgPos="center"
+      textColor="white"
     >
       <Text
         fontSize="sm"
         fontFamily="lit"
-        fontWeight="thin"
+        fontWeight="bold"
         letterSpacing="widest"
         lineHeight="short"
         textAlign="center"
-        mt={20}
+        mt={8}
       >
         GET IN TOUCH
       </Text>
-      <Text>
+      <Text
+        fontFamily="new"
+        fontSize="2xl"
+      >
         We&apos;d love to speak with you.
       </Text>
       <Text>
         Please call us at 800-947-6438, email us at info@gtsavingsbank.net or click the button below to access our contact form.
       </Text>
-      <Button>
+      <Button
+        m={5}
+      >
         Contact Us
       </Button>
     </Box>
