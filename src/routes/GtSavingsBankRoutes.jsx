50d7f40317e77a5c019cb300fa72cd3c61f0961c
julzedz <edoziechukwuma@gmail.com>
2023-12-13 22:42:17 +0100

    Refactor GtSavingsBankRoutes to conditionally show Navbar and Sidebar based on location

--- src/routes/GtSavingsBankRoutes.jsx
+++ src/routes/GtSavingsBankRoutes.jsx
@@ -1,4 +1,6 @@
-import { Routes, Navigate, Route } from 'react-router';
+import {
+  Routes, Navigate, Route, useLocation,
+} from 'react-router';
 import Navbar from '../components/navbar';
 import Home from '../components/home';
 import Banking from '../components/banking';
@@ -11,28 +13,34 @@ import Login from '../components/login';
 import Footer from '../components/footer';
 import Sidebar from '../components/sidebar';
 
-const GtSavingsBankRoutes = () => (
-  <>
-    <div className="app-container">
-      <Navbar />
-      ||
-      <Sidebar />
-      <div className="tabs-container">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/banking" element={<Banking />} />
-          <Route path="/investment" element={<Investment />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/insurance" element={<Insurance />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+const GtSavingsBankRoutes = () => {
+  const location = useLocation();
+  const showNavbar = ['/home', '/banking', '/investment', '/support', '/insurance', '/login'].includes(location.pathname);
+  const showSidebar = ['/account', '/profile', '/investment', '/transactions', '/settings'].includes(location.pathname);
+
+  return (
+    <>
+      <div className="app-container">
+        <Navbar />
+        ||
+        <Sidebar />
+        <div className="tabs-container">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/banking" element={<Banking />} />
+            <Route path="/investment" element={<Investment />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/insurance" element={<Insurance />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default GtSavingsBankRoutes;
