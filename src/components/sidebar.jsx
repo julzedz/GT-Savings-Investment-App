07d8bc23a8957c7346f59e0e8876c32f4c861741
julzedz <edoziechukwuma@gmail.com>
2023-12-14 20:14:55 +0100

    Add sidebar component with basic layout

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,42 @@
+import React, { useState } from 'react';
+import {
+  Flex,
+  Text,
+  IconButton,
+  Divider,
+  Avatar,
+  Heading,
+} from '@chakra-ui/react';
+import {
+  FiMenu,
+  FiHome,
+  FiCalendar,
+  FiUser,
+  FiDollarSign,
+} from 'react-icons/fi';
+import { IoPawOutline } from 'react-icons/io5';
+import NavItem from '../components/navitem';
+
 const Sidebar = () => (
-  <h2>
-    Hello Sidebar
-  </h2>
+  <Flex
+    pos="sticky"
+    left="5"
+    h="95vh"
+    mt="2.5vh"
+    boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+    w="200px"
+    flexDir="column"
+    justifyContent="space-between"
+  >
+    <Flex></Flex>
+    <Flex
+      p="5%"
+      flexDir="column"
+      w="100%"
+    >
+
+    </Flex>
+  </Flex>
 );
 
 export default Sidebar;
