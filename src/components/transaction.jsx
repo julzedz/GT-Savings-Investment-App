662e1d9853ed416efbd0ad119b1ba8ae0d0f0d1d
julzedz <edoziechukwuma@gmail.com>
2023-12-16 04:30:00 +0100

    Update transaction table layout

--- src/components/transaction.jsx
+++ src/components/transaction.jsx
@@ -19,8 +19,9 @@ const Transaction = () => (
                 <Th>Transaction ID</Th>
                 <Th>Name</Th>
                 <Th>Transaction Type</Th>
-                <Th>Amount</Th>
                 <Th>Date</Th>
+                <Th>Status</Th>
+                <Th>Amount</Th>
               </Tr>
             </Thead>
             <Tbody>
@@ -28,36 +29,121 @@ const Transaction = () => (
                 <Td>874201569</Td>
                 <Td>Clifford Mgbeojikwe</Td>
                 <Td>Deposit</Td>
-                <Td>$1250.00</Td>
                 <Td>1/1/2021</Td>
+                <Td>Completed</Td>
+                <Td>$1250.00</Td>
               </Tr>
               <Tr>
                 <Td>326987415</Td>
                 <Td>Hyginus Mbonu</Td>
                 <Td>Withdrawal</Td>
-                <Td>$55,000.00</Td>
                 <Td>1/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$55,000.00</Td>
               </Tr>
               <Tr>
                 <Td>590134682</Td>
                 <Td>Ozumba Mbadiwe</Td>
                 <Td>Deposit</Td>
-                <Td>$15,340.00</Td>
                 <Td>1/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$15,340.00</Td>
               </Tr>
               <Tr>
                 <Td>712465839</Td>
                 <Td>Ojo Maduekwe</Td>
                 <Td>Withdrawal</Td>
-                <Td>$5,000.00</Td>
                 <Td>11/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$5,000.00</Td>
               </Tr>
               <Tr>
                 <Td>945803216</Td>
                 <Td>Sabinus Okafo</Td>
                 <Td>Withdrawal</Td>
-                <Td>$3,720.00</Td>
                 <Td>13/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$3,720.00</Td>
+              </Tr>
+              <Tr>
+                <Td>874201569</Td>
+                <Td>Clifford Mgbeojikwe</Td>
+                <Td>Deposit</Td>
+                <Td>1/1/2021</Td>
+                <Td>Completed</Td>
+                <Td>$1250.00</Td>
+              </Tr>
+              <Tr>
+                <Td>326987415</Td>
+                <Td>Hyginus Mbonu</Td>
+                <Td>Withdrawal</Td>
+                <Td>1/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$55,000.00</Td>
+              </Tr>
+              <Tr>
+                <Td>590134682</Td>
+                <Td>Ozumba Mbadiwe</Td>
+                <Td>Deposit</Td>
+                <Td>1/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$15,340.00</Td>
+              </Tr>
+              <Tr>
+                <Td>712465839</Td>
+                <Td>Ojo Maduekwe</Td>
+                <Td>Withdrawal</Td>
+                <Td>11/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$5,000.00</Td>
+              </Tr>
+              <Tr>
+                <Td>945803216</Td>
+                <Td>Sabinus Okafo</Td>
+                <Td>Withdrawal</Td>
+                <Td>13/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$3,720.00</Td>
+              </Tr>
+              <Tr>
+                <Td>874201569</Td>
+                <Td>Clifford Mgbeojikwe</Td>
+                <Td>Deposit</Td>
+                <Td>1/1/2021</Td>
+                <Td>Completed</Td>
+                <Td>$1250.00</Td>
+              </Tr>
+              <Tr>
+                <Td>326987415</Td>
+                <Td>Hyginus Mbonu</Td>
+                <Td>Withdrawal</Td>
+                <Td>1/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$55,000.00</Td>
+              </Tr>
+              <Tr>
+                <Td>590134682</Td>
+                <Td>Ozumba Mbadiwe</Td>
+                <Td>Deposit</Td>
+                <Td>1/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$15,340.00</Td>
+              </Tr>
+              <Tr>
+                <Td>712465839</Td>
+                <Td>Ojo Maduekwe</Td>
+                <Td>Withdrawal</Td>
+                <Td>11/2/2021</Td>
+                <Td>Completed</Td>
+                <Td>$5,000.00</Td>
+              </Tr>
+              <Tr>
+                <Td>945803216</Td>
+                <Td>Sabinus Okafo</Td>
+                <Td>Withdrawal</Td>
+                <Td>13/5/2021</Td>
+                <Td>Completed</Td>
+                <Td>$3,720.00</Td>
               </Tr>
             </Tbody>
           </Table>
