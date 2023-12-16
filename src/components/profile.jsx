8d7b34e3e9586abbe8707f75772eadaa51205b93
julzedz <edoziechukwuma@gmail.com>
2023-12-16 17:14:44 +0100

    Refactor profile component to include sidebar

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,12 +1,28 @@
+import { Flex } from '@chakra-ui/react';
+import Sidebar from './sidebar';
+
 const Profile = () => (
   <>
-    <h2>Profile </h2>
-    <h3>Inbox</h3>
-    <h3>Notifications</h3>
-    <h3>Email</h3>
-    <h3>Transfer</h3>
-    <h3>Settings</h3>
-    <h3>Login settings</h3>
+    <Flex
+      bgColor="gunmetal"
+      color="white"
+    >
+      <Sidebar />
+      <Flex
+        flexDir="column"
+        pos="relative"
+        left="80"
+        minHeight="3xl"
+      >
+        <h2>Profile </h2>
+        <h3>Inbox</h3>
+        <h3>Notifications</h3>
+        <h3>Email</h3>
+        <h3>Transfer</h3>
+        <h3>Settings</h3>
+        <h3>Login settings</h3>
+      </Flex>
+    </Flex>
   </>
 );
 
