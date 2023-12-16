443270df1d1ec66645ab7e1cf6c958925d5b0695
julzedz <edoziechukwuma@gmail.com>
2023-12-16 17:14:33 +0100

    Add sidebar and styling to Investment component

--- src/components/investment.jsx
+++ src/components/investment.jsx
@@ -1,8 +1,24 @@
+import { Flex } from '@chakra-ui/react';
+import Sidebar from './sidebar';
+
 const Investment = () => (
   <>
-    <h2>Investment Services</h2>
-    <h3>Investments</h3>
-    <h3>Insurance</h3>
+    <Flex
+      bgColor="gunmetal"
+      color="white"
+    >
+      <Sidebar />
+      <Flex
+        flexDir="column"
+        pos="relative"
+        left="80"
+        minHeight="3xl"
+      >
+        <h2>Investment Services</h2>
+        <h3>Investments</h3>
+        <h3>Insurance</h3>
+      </Flex>
+    </Flex>
   </>
 );
 
