f240aa95ba452d1c54820fe9235e9f8d59d190bf
julzedz <edoziechukwuma@gmail.com>
2023-12-18 18:14:03 +0100

    Add new components and update styles in dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,6 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex, Text, Box, Divider,
+} from '@chakra-ui/react';
 import Sidebar from './sidebar';
 
 const Dashboard = () => (
@@ -14,11 +16,27 @@ const Dashboard = () => (
         flexDir="column"
         minHeight="3xl"
         p={5}
-        pos="relative"
-        left={{ base: 20, md: 80 }}
+        flex="1"
+        marginLeft={{ base: 20, md: '21rem' }}
         overflowY="scroll"
+        bgColor="green"
       >
-        <h2>Dashboard</h2>
+        <Flex bgColor="blue" alignItems="center" justifyContent="flex-start">
+          <Box fontFamily="lit" pr={5}>
+            Welcome Firstname!
+          </Box>
+          <Divider />
+          <Flex>
+            <Flex m={0} flexDir="column">
+              <Text m={0}>User ID</Text>
+              <Text>123456789</Text>
+            </Flex>
+            <Flex m={0} flexDir="column">
+              <Text m={0}>User Type</Text>
+              <Text>Personal</Text>
+            </Flex>
+          </Flex>
+        </Flex>
         <h3>My Wallet</h3>
         <h5>Hi User</h5>
         <h5>$355,760.00</h5>
