6d1a4f40d5840f8cc4a545c78854a9fb44f80b93
julzedz <edoziechukwuma@gmail.com>
2023-12-19 21:58:08 +0100

    Add buttons and styling to dashboard component

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,5 @@
 import {
-  Flex, Text, Divider,
+  Flex, Text, Divider, Button,
 } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
@@ -8,8 +8,9 @@ import Sidebar from './sidebar';
 const Dashboard = () => (
   <>
     <Flex
-      bgColor="gunmetal"
-      color="white"
+      // bgColor="gunmetal"
+      // color="white"
+      color="black"
       w="100%"
     >
       <Sidebar />
@@ -39,18 +40,67 @@ const Dashboard = () => (
             </Flex>
           </Flex>
         </Flex>
-        <Flex>
-          <Flex>
-            <Text> Estimated Balance</Text>
-            <ViewIcon />
-            <ViewOffIcon display="none" />
+        <Flex
+          flexDir="column"
+          // bgColor="blue"
+          maxWidth="60rem"
+          borderRadius="2xl"
+          borderWidth="1px"
+          borderColor="#eaecef"
+          p={6}
+          mb={6}
+        >
+          <Flex justifyContent="space-between">
+            <Flex p={0} gap={1.5} alignItems="center" mb={0.5} justifyContent="space-evenly">
+              <Text m={0} fontSize="xl" fontWeight="semibold"> Estimated Balance</Text>
+              <ViewIcon />
+              <ViewOffIcon display="none" />
+            </Flex>
+            <Flex p={0} m={0} gap={3}>
+              <Button
+                fontSize="sm"
+                fontWeight="600"
+                lineHeight="short"
+                colorScheme="gray"
+                size="sm"
+                variant="solid"
+              >
+                Deposit
+              </Button>
+              <Button
+                colorScheme="gray"
+                size="sm"
+                variant="solid"
+                fontSize="sm"
+                fontWeight="600"
+                lineHeight="short"
+              >
+                Withdraw
+              </Button>
+              <Button
+                colorScheme="gray"
+                size="sm"
+                variant="solid"
+                fontSize="sm"
+                fontWeight="600"
+                lineHeight="short"
+              >
+                Transfer
+              </Button>
+            </Flex>
+          </Flex>
+          <Flex width="fit-content">
+            <Text fontSize="2rem" fontWeight="semibold" m={0}>355,760.32</Text>
+            <Text fontSize="sm" fontWeight="semibold" lineHeight="short" m={0} ml={2} alignSelf="flex-end" pb={2}>USD</Text>
+          </Flex>
+          <Flex flexDir="column" mt={3}>
+            <Text fontSize="sm" lineHeight="short" mb={3}>≈ 8.2734883 BTC</Text>
+            <Text fontSize="sm" lineHeight="short" mb={3}>
+              Today&apos;s PnL
+              <Text display="inline" ml="3" color="green">+ $152.50(0.2%)</Text>
+            </Text>
           </Flex>
         </Flex>
-        <h3>My Wallet</h3>
-        <h5>Hi User</h5>
-        <h5>$355,760.00</h5>
-        <h3>Send a payment</h3>
-        <h3>Request a payment</h3>
         <h3>Transaction history</h3>
       </Flex>
     </Flex>
