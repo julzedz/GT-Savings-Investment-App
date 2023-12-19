a413f8796b36d595249fdc1e7d24b5640af61a22
julzedz <edoziechukwuma@gmail.com>
2023-12-19 01:20:12 +0100

    Refactor dashboard component layout and styles

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,5 @@
 import {
-  Flex, Text, Box, Divider,
+  Flex, Text, Divider,
 } from '@chakra-ui/react';
 import Sidebar from './sidebar';
 
@@ -19,21 +19,21 @@ const Dashboard = () => (
         flex="1"
         marginLeft={{ base: 20, md: '21rem' }}
         overflowY="scroll"
-        bgColor="green"
+        fontFamily="noto"
       >
-        <Flex bgColor="blue" alignItems="center" justifyContent="flex-start">
-          <Box fontFamily="lit" pr={8}>
-            Welcome Firstname!
-          </Box>
-          <Divider orientation="vertical" />
-          <Flex px={8} gap={8} bgColor="darkgrey">
-            <Flex m={0} flexDir="column">
+        <Flex mb={8} alignItems="center" justifyContent="flex-start">
+          <Text m={0} fontSize="2xl" fontWeight="medium" pr={8}>
+            Welcome Rob!
+          </Text>
+          <Divider height="80%" color="lightgray" orientation="vertical" />
+          <Flex fontSize="sm" lineHeight="shorter" alignItems="center" px={8}>
+            <Flex alignItems="center" justifyContent="center" m={0} mr={12} flexDir="column">
               <Text m={0}>User ID</Text>
-              <Text>123456789</Text>
+              <Text m={0}>123456789</Text>
             </Flex>
-            <Flex m={0} flexDir="column">
+            <Flex alignItems="center" justifyContent="center" m={0} mr={12} flexDir="column">
               <Text m={0}>User Type</Text>
-              <Text>Personal</Text>
+              <Text m={0}>Personal</Text>
             </Flex>
           </Flex>
         </Flex>
