03da081b6dd36fdbb477573961b65a1089b606e0
julzedz <edoziechukwuma@gmail.com>
2023-12-23 16:55:10 +0100

    Add user information section to transaction page

--- src/components/transaction.jsx
+++ src/components/transaction.jsx
@@ -1,5 +1,5 @@
 import {
-  Flex, Box, Text, Table, Thead, Tbody, Tr, Th, Td,
+  Flex, Divider, Box, Text, Table, Thead, Tbody, Tr, Th, Td,
 } from '@chakra-ui/react';
 import Sidebar from './sidebar';
 
@@ -21,8 +21,24 @@ const Transaction = () => (
         marginLeft={{ base: 20, md: '21rem' }}
         fontFamily="noto"
       >
+        <Flex flexDir={{ base: 'column', lg: 'row' }} mb={8} alignItems="center" justifyContent="flex-start">
+          <Text m={0} alignSelf="flex-start" fontSize="2xl" fontWeight="medium" pr={8} pb={{ base: 4, lg: 0 }}>
+            Welcome Rob! 👋
+          </Text>
+          <Divider display={{ base: 'none', lg: 'inline' }} w="1px" color="#eaecef" orientation="vertical" />
+          <Flex w={{ base: '100%', lg: 'auto' }} flexDir={{ base: 'column', lg: 'row' }} fontSize="sm" lineHeight="shorter" alignItems="center" px={{ base: 0, lg: 8 }}>
+            <Flex w={{ base: 'inherit', lg: 'auto' }} alignItems="center" justifyContent={{ base: 'space-between', lg: 'center' }} m={0} mr={{ base: 0, lg: 12 }} flexDir={{ base: 'row', lg: 'column' }}>
+              <Text color="#929aa5" m={0} mb={1}>User ID</Text>
+              <Text m={0}>123456789</Text>
+            </Flex>
+            <Flex w={{ base: 'inherit', lg: 'auto' }} alignItems="center" justifyContent={{ base: 'space-between', lg: 'center' }} m={0} mr={{ base: 0, lg: 12 }} flexDir={{ base: 'row', lg: 'column' }}>
+              <Text color="#929aa5" m={0} mb={1}>User Type</Text>
+              <Text m={0}>Personal</Text>
+            </Flex>
+          </Flex>
+        </Flex>
         <Box>
-          <Text>Transactions</Text>
+          <Text>Transaction History</Text>
           <Table>
             <Thead>
               <Tr>
