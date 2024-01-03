4a43f537e69f8a07c40d71255c907b54364ced0b
julzedz <edoziechukwuma@gmail.com>
2024-01-03 10:55:28 +0100

    Update copyright year to 2024

--- src/components/deposit.jsx
+++ src/components/deposit.jsx
@@ -1,22 +1,28 @@
 import { Flex } from '@chakra-ui/react';
 import Sidebar from './sidebar';
+import AccountFooter from './accountfooter';
 
 const Deposit = () => (
-  <Flex>
-    <Sidebar />
-    <Flex
-      ml={4}
-      flexDir="column"
-      minHeight="3xl"
-      p={5}
-      flex="1"
-      marginLeft={{ base: 20, md: '21rem' }}
-      fontFamily="noto"
-    >
-      <h2>Deposit Services</h2>
-      <h3>Deposit</h3>
+  <>
+    <Flex>
+      <Sidebar />
+      <Flex
+        ml={4}
+        flexDir="column"
+        minHeight="3xl"
+        p={5}
+        flex="1"
+        marginLeft={{ base: 20, md: '21rem' }}
+        fontFamily="noto"
+      >
+        <Flex minHeight="3xl" flexDir="column">
+          <h2>Deposit Services</h2>
+          <h3>Deposit</h3>
+        </Flex>
+        <AccountFooter />
+      </Flex>
     </Flex>
-  </Flex>
+  </>
 );
 
 export default Deposit;
