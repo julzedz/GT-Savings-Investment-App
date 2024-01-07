5c5a8e6f6d1cdc6c2ba5491eeb03bd3b86d1689c
julzedz <edoziechukwuma@gmail.com>
2024-01-07 16:32:38 +0100

    Add Tether icon and deposit address tooltip

--- src/components/deposit.jsx
+++ src/components/deposit.jsx
@@ -1,9 +1,12 @@
 import {
-  Flex, Text, FormControl, Select, FormLabel, NumberInput, NumberInputField,
-  NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper,
+  Flex, Text, FormControl, Select, FormLabel, NumberInput, NumberInputField, Icon,
+  NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, UnorderedList,
+  ListItem, Tooltip,
 } from '@chakra-ui/react';
+import { FaCopy } from 'react-icons/fa';
 import Sidebar from './sidebar';
 import AccountFooter from './accountfooter';
+// import Tether from '../assets/icons8-tether.svg';
 
 const Deposit = () => (
   <>
@@ -36,6 +39,11 @@ const Deposit = () => (
               placeholder="Select payment method"
             >
               <option value="USDT">USDT</option>
+              <a href="https://icons8.com/icon/U8V97McJaXmr/tether">Tether</a>
+              icon by
+              <a href="https://icons8.com">
+                Icons8
+              </a>
               <option value="USDC">USDC</option>
               <option value="BTC">BTC</option>
               <option value="ETH">Ethereum</option>
@@ -48,12 +56,32 @@ const Deposit = () => (
           <FormControl w="60%" mb={6}>
             <FormLabel fontSize="xs">Amount</FormLabel>
             <NumberInput step={1000} min={1000}>
-              <NumberInputField placeholder="Minimum ≈ 1000 USDT" />
+              <NumberInputField placeholder="Min. deposit 1000 USDT" />
               <NumberInputStepper>
                 <NumberIncrementStepper />
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
+          </FormControl>
+
+          <Text>Note</Text>
+          <UnorderedList fontSize="xs" spacing={3} mb={6}>
+            <ListItem>Please make deposit before submitting the form.</ListItem>
+            <ListItem>
+              Deposits will be credited and available on dashboard after 1 confirmation.
+            </ListItem>
+            <ListItem>Crypto deposits are faster and preferable.</ListItem>
+            <ListItem>Do not send NFTs to this address.</ListItem>
+          </UnorderedList>
+
+          <FormControl w="60%" mb={6}>
+            <FormLabel fontSize="xs">Deposit Address</FormLabel>
+            <Text fontSize="14px" lineHeight="20px" fontWeight="500" color="#1e2329" wordBreak="break-word" textAlign="start">
+              0x3bF71E4250631076269426d735F4Ea37c10C7256
+            </Text>
+            <Tooltip label="Click to copy" fontFamily="new" fontSize="xs">
+              <Icon as={FaCopy} cursor="pointer" ml={1} display="inline" />
+            </Tooltip>
           </FormControl>
         </Flex>
         <AccountFooter />
