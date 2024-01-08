99c2a869006193366bcc73e12717ee65249171c8
julzedz <edoziechukwuma@gmail.com>
2024-01-08 04:40:58 +0100

    Add copy to clipboard functionality

--- src/components/deposit.jsx
+++ src/components/deposit.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
   Flex, Text, FormControl, Select, FormLabel, NumberInput, NumberInputField, Icon,
   NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, UnorderedList,
@@ -9,96 +10,109 @@ import AccountFooter from './accountfooter';
 import qrcode from '../assets/qrcode.jpg';
 // import Tether from '../assets/icons8-tether.svg';
 
-const Deposit = () => (
-  <>
-    <Flex>
-      <Sidebar />
-      <Flex
-        ml={4}
-        flexDir="column"
-        minHeight="3xl"
-        p={5}
-        flex="1"
-        marginLeft={{ base: 20, md: '21rem' }}
-        fontFamily="noto"
-      >
-        <Flex minHeight="3xl" flexDir="column">
-          <Text
-            alignSelf="center"
-            justifySelf="center"
-            fontSize={{ base: 'lg', xl: '2xl' }}
-            fontWeight="bold"
-            mb={6}
-          >
-            Fund Account
-          </Text>
-          <FormControl w="60%" mb={6}>
-            <FormLabel fontSize="xs">Payment Method</FormLabel>
-            <Select
-              // value=""
-              id="payment"
-              placeholder="Select payment method"
+const Deposit = () => {
+  const address = '0x3bF71E4250631076269426d735F4Ea37c10C7256';
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(address);
+      console.log('Address copied to clipboard');
+    } catch (err) {
+      console.log('Failed to copy address', err);
+    }
+  };
+
+  return (
+    <>
+      <Flex>
+        <Sidebar />
+        <Flex
+          ml={4}
+          flexDir="column"
+          minHeight="3xl"
+          p={5}
+          flex="1"
+          marginLeft={{ base: 20, md: '21rem' }}
+          fontFamily="noto"
+        >
+          <Flex minHeight="3xl" flexDir="column">
+            <Text
+              alignSelf="center"
+              justifySelf="center"
+              fontSize={{ base: 'lg', xl: '2xl' }}
+              fontWeight="bold"
+              mb={6}
             >
-              <option value="USDT">USDT</option>
-              <a href="https://icons8.com/icon/U8V97McJaXmr/tether">Tether</a>
-              icon by
-              <a href="https://icons8.com">
-                Icons8
-              </a>
-              <option value="USDC">USDC</option>
-              <option value="BTC">BTC</option>
-              <option value="ETH">Ethereum</option>
-              <option value="cashapp">Cashapp</option>
-              <option value="venmo">Venmo</option>
-              <option value="paypal">PayPal</option>
-              <option value="zelle">Zelle</option>
-            </Select>
-          </FormControl>
-          <FormControl w="60%" mb={6}>
-            <FormLabel fontSize="xs">Amount</FormLabel>
-            <NumberInput step={1000} min={1000}>
-              <NumberInputField placeholder="Min. deposit 1000 USDT" />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
+              Fund Account
+            </Text>
+            <FormControl w="60%" mb={6}>
+              <FormLabel fontSize="xs">Payment Method</FormLabel>
+              <Select
+              // value=""
+                id="payment"
+                placeholder="Select payment method"
+              >
+                <option value="USDT">USDT</option>
+                <a href="https://icons8.com/icon/U8V97McJaXmr/tether">Tether</a>
+                icon by
+                <a href="https://icons8.com">
+                  Icons8
+                </a>
+                <option value="USDC">USDC</option>
+                <option value="BTC">BTC</option>
+                <option value="ETH">Ethereum</option>
+                <option value="cashapp">Cashapp</option>
+                <option value="venmo">Venmo</option>
+                <option value="paypal">PayPal</option>
+                <option value="zelle">Zelle</option>
+              </Select>
+            </FormControl>
+            <FormControl w="60%" mb={6}>
+              <FormLabel fontSize="xs">Amount</FormLabel>
+              <NumberInput step={1000} min={1000}>
+                <NumberInputField placeholder="Min. deposit 1000 USDT" />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
 
-          <Text>Note</Text>
-          <UnorderedList fontSize="xs" spacing={3} mb={6}>
-            <ListItem>Please make deposit before submitting the form.</ListItem>
-            <ListItem>
-              Deposits will be credited and available on dashboard after 1 confirmation.
-            </ListItem>
-            <ListItem>Crypto deposits are faster and preferable.</ListItem>
-            <ListItem>Do not send NFTs to this address.</ListItem>
-          </UnorderedList>
+            <Text>Note</Text>
+            <UnorderedList fontSize="xs" spacing={3} mb={6}>
+              <ListItem>Please make deposit before submitting the form.</ListItem>
+              <ListItem>
+                Deposits will be credited and available on dashboard after 1 confirmation.
+              </ListItem>
+              <ListItem>Crypto deposits are faster and preferable.</ListItem>
+              <ListItem>Do not send NFTs to this address.</ListItem>
+            </UnorderedList>
 
-          <FormControl w="60%" mb={6}>
-            <FormLabel fontSize="xs">Deposit Address</FormLabel>
-            <Flex alignItems="center" justifyContent="space-between" bgColor="#fafafa" borderRadius="5px" p={5} minH="10rem" gap="5%" w="100%">
-              <Flex alignItems="center" justifyContent="center">
-                <Image src={qrcode} boxSize="100px" />
-              </Flex>
-              <Flex alignItems="center" justifyContent="center">
-                <Flex flexDir="column">
-                  <Text fontSize="xs" color="gray.500" m={0}>Address</Text>
-                  <Text fontSize="md" lineHeight="20px" fontWeight="500" color="#1e2329" wordBreak="break-word" textAlign="start" textOverflow="ellipsis">
-                    0x3bF71E4250631076269426d735F4Ea37c10C7256
-                  </Text>
+            <FormControl w="60%" mb={6}>
+              <FormLabel fontSize="xs">Deposit Address</FormLabel>
+              <Flex alignItems="center" justifyContent="space-between" bgColor="#fafafa" borderRadius="5px" p={5} minH="10rem" gap="5%" w="100%">
+                <Flex alignItems="center" justifyContent="center">
+                  <Image src={qrcode} boxSize="100px" />
                 </Flex>
-                <Tooltip label="Click to copy" fontFamily="new" fontSize="xs">
-                  <Icon as={FaCopy} color="gray" _hover={{ color: 'black' }} cursor="pointer" ml={1} display="inline" />
-                </Tooltip>
+                <Flex alignItems="center" justifyContent="center">
+                  <Flex flexDir="column">
+                    <Text fontSize="xs" color="gray.500" m={0}>Address</Text>
+                    <Text fontSize="md" lineHeight="20px" fontWeight="500" color="#1e2329" wordBreak="break-word" textAlign="start" textOverflow="ellipsis">
+                      {address}
+                    </Text>
+                  </Flex>
+                  <Tooltip label="Click to copy" fontFamily="new" fontSize="xs">
+                    <Icon as={FaCopy} padding="2px" borderRadius="2px" bgColor="#e9ecef" color="gray" _hover={{ color: 'black' }} cursor="pointer" ml={2} display="inline" onClick={handleCopy} />
+                  </Tooltip>
+                </Flex>
               </Flex>
-            </Flex>
-          </FormControl>
+            </FormControl>
+          </Flex>
+          <AccountFooter />
         </Flex>
-        <AccountFooter />
       </Flex>
-    </Flex>
-  </>
-);
+    </>
+  );
+};
 
 export default Deposit;
