c4ac47ab41e292031714576e1d4d07f778f6409b
julzedz <edoziechukwuma@gmail.com>
2024-01-11 17:45:44 +0100

    Add investment plans section with background image and button

--- src/components/investmentplans.jsx
+++ src/components/investmentplans.jsx
@@ -1,7 +1,55 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Box, Heading, Flex, Text, Button, Image,
+} from '@chakra-ui/react';
+import building from '../assets/building2.jpg';
 
 const InvestmentPlans = () => (
-  <Flex minHeight={72}>Hello World!</Flex>
+  <>
+    <Box
+      display="flex"
+      mt={{ base: 13, lg: 0 }}
+      minH={{ base: '10rem', lg: '2xs' }}
+      maxH={{ base: 'xs', lg: 'xs' }}
+      backgroundImage={`linear-gradient(to right, #000000 25%, #0087d430 100%), url(${building})`}
+      bgSize="cover"
+      opacity="0.93"
+      bgColor="transparent"
+      alignItems="flex-end"
+    >
+      <Heading
+        m={3}
+        p={4}
+        textAlign={{ base: 'center', lg: 'left' }}
+        fontSize={{ base: '5xl', lg: '7xl' }}
+        fontWeight={{ base: 'bold', lg: 'black' }}
+        width={{ base: '100%', lg: '80%' }}
+        textColor="white"
+      >
+        Investment Plans
+      </Heading>
+    </Box>
+    <Box>
+      <Flex>
+        <Text>Maximize Your Returns</Text>
+        <Text>With Our Investment Products</Text>
+        <Button
+          mt={4}
+          colorScheme="green"
+                // isLoading={props.isSubmitting}
+              // disabled={props.isSubmitting}
+          type="submit"
+          fontFamily="noto"
+          w="100%"
+          p={6}
+        >
+          Click To Invest
+        </Button>
+      </Flex>
+      <Flex>
+        <Image src={} boxSize={20}/>
+      </Flex>
+    </Box>
+  </>
 );
 
 export default InvestmentPlans;
