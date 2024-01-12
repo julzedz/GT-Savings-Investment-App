0d833c236d89a110d4f61d846b6e68c22c546d4a
julzedz <edoziechukwuma@gmail.com>
2024-01-12 19:48:56 +0100

    Add investment plans section and images

--- src/components/investmentplans.jsx
+++ src/components/investmentplans.jsx
@@ -30,25 +30,23 @@ const InvestmentPlans = () => (
       </Heading>
     </Box>
     <Flex w="100%" bgColor="applegreen" justifyContent="center" alignItems="center" flexDir={{ base: 'column', slg: 'row' }}>
-      <Flex w={{ base: '100%', slg: '50%' }} p={{ base: 6, slg: 0 }} order={{ base: 2, slg: 1 }} textAlign={{ base: 'center', slg: 'left' }} justifySelf="center" flexDir="column" pl={9}>
+      <Flex w={{ base: '100%', slg: '50%' }} py={{ base: 6, slg: 0 }} px={{ base: 6, slg: 'auto' }} pl={9} order={{ base: 2, slg: 1 }} textAlign={{ base: 'center', slg: 'left' }} justifySelf="center" flexDir="column">
         <Heading
           fontSize={{
             base: '', md: '2rem', slg: '3rem', xl: '4rem',
           }}
-          lineHeight="122%"
+          lineHeight="102%"
           maxW={{ base: '100%', slg: '455px' }}
           m={0}
           color="gunmetal"
         >
           Maximize Your Returns
         </Heading>
-        <Heading mt="0.3rem" color="white" lineHeight={{ base: '1.5em', slg: '4.2em' }} fontSize={{ base: '1.3em', slg: '1.7rem' }}>With Our Investment Products</Heading>
+        <Heading mt="0.3rem" color="white" lineHeight={{ base: '1.5em', slg: '2.3em' }} fontSize={{ base: '1.3em', slg: '1.7rem' }}>With Our Investment Products</Heading>
         <Button
-          mt={4}
+          // mt={4}
           colorScheme="gray"
           alignSelf={{ base: 'center', slg: 'auto' }}
-                // isLoading={props.isSubmitting}
-              // disabled={props.isSubmitting}
           type="submit"
           fontFamily="noto"
           w="30%"
@@ -57,8 +55,8 @@ const InvestmentPlans = () => (
           Click To Invest
         </Button>
       </Flex>
-      <Flex w={{ base: '', slg: '50%' }} order={{ base: 1, slg: 2 }}>
-        <Image objectFit="cover" w="100%" src={moneytree} />
+      <Flex w={{ base: '100%', slg: '50%' }} order={{ base: 1, slg: 2 }}>
+        <Image maxH={{ base: 'xs', slg: 'auto' }} objectFit="cover" w="100%" src={moneytree} />
       </Flex>
     </Flex>
   </>
