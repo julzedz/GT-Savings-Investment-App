273f43fbbafdd5ebe475d107c82355bb434315d2
julzedz <edoziechukwuma@gmail.com>
2024-01-21 17:44:41 +0100

    Update font size in AccountFooter component

--- src/components/accountfooter.jsx
+++ src/components/accountfooter.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 const AccountFooter = () => (
-  <Box textAlign="center" pt={4} pb={3} fontFamily="heading" fontSize="sm" color="gunmetal">
+  <Box textAlign="center" pt={4} pb={3} fontFamily="heading" fontSize={{ base: 'xs', sm: 'sm' }} color="gunmetal">
     <span>
       {new Date().getFullYear()}
     </span>
