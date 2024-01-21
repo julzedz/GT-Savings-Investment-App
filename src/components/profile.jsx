5cbd910f6888496da25c87d497031fe36e8d5983
julzedz <edoziechukwuma@gmail.com>
2024-01-21 12:04:05 +0100

    Add profile information and update layout

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Divider, Text } from '@chakra-ui/react';
 import Sidebar from './sidebar';
 
 const Profile = () => (
@@ -9,20 +9,31 @@ const Profile = () => (
     >
       <Sidebar />
       <Flex
-        flexDir="column"
-        pos="relative"
-        p={5}
-        left={{ base: 20, md: 80 }}
-        minHeight="3xl"
         ml={4}
+        flexDir="column"
+        minHeight="3xl"
+        p={5}
         flex="1"
         marginLeft={{ base: 20, md: '21rem' }}
         overflowY="scroll"
         fontFamily="noto"
       >
-        <h2>Profile </h2>
-        <h3>Inbox</h3>
-        <h3>Notifications</h3>
+        <Flex flexDir={{ base: 'column', lg: 'row' }} mb={8} alignItems="center" justifyContent="flex-start">
+          <Text m={0} alignSelf="flex-start" fontSize="2xl" fontWeight="medium" pr={8} pb={{ base: 4, lg: 0 }}>
+            Profile
+          </Text>
+          <Divider display={{ base: 'none', lg: 'inline' }} w="1px" color="#eaecef" orientation="vertical" />
+          <Flex w={{ base: '100%', lg: 'auto' }} flexDir={{ base: 'column', lg: 'row' }} fontSize="sm" lineHeight="shorter" alignItems="center" px={{ base: 0, lg: 8 }}>
+            <Flex w={{ base: 'inherit', lg: 'auto' }} alignItems="center" justifyContent={{ base: 'space-between', lg: 'center' }} m={0} mr={{ base: 0, lg: 12 }} flexDir={{ base: 'row', lg: 'column' }}>
+              <Text color="#929aa5" m={0} mb={1}>User ID</Text>
+              <Text m={0}>123456789</Text>
+            </Flex>
+            <Flex w={{ base: 'inherit', lg: 'auto' }} alignItems="center" justifyContent={{ base: 'space-between', lg: 'center' }} m={0} mr={{ base: 0, lg: 12 }} flexDir={{ base: 'row', lg: 'column' }}>
+              <Text color="#929aa5" m={0} mb={1}>User Type</Text>
+              <Text m={0}>Personal</Text>
+            </Flex>
+          </Flex>
+        </Flex>
         <h3>Email</h3>
         <h3>Transfer</h3>
         <h3>Settings</h3>
