deccbf0e371f20e88315c74f0811a660bb33879b
julzedz <edoziechukwuma@gmail.com>
2024-01-26 16:48:01 +0100

    Add request interceptor to include authorization token

--- src/api.js
+++ src/api.js
@@ -4,4 +4,16 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    const modifiedConfig = { ...config }; // Create a copy of config
+    if (token) {
+      modifiedConfig.headers.Authorization = `Bearer ${token}`;
+    }
+    return modifiedConfig;
+  },
+  (error) => Promise.reject(error),
+);
+
 export default api;
