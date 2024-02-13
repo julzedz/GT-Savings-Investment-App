aa7edcc44d6db7599c91f11a41f532a649937b07
julzedz <edoziechukwuma@gmail.com>
2024-02-13 15:52:44 +0100

    Add conditional rendering for Login button based on token presence

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
   const [isOpen, setOpen] = useState(false);
+  const token = localStorage.getItem('token');
 
   return (
     <>
@@ -58,22 +59,24 @@ const Navbar = () => {
         </Box>
 
         {/* Login */}
-        <Button
-          as={reactrouterlink}
-          to="/login"
-          order={{ base: 2, slg: 3 }}
-          bg="applegreen"
-          alignItems="center"
-          height="auto"
-          color="white"
-          px={4}
-          py={3}
-          _hover={{ bg: 'applegreen', color: 'black' }}
-          fontFamily="new"
-          display={{ base: 'block', xl: 'none' }}
-        >
-          Login
-        </Button>
+        {!token && (
+          <Button
+            as={reactrouterlink}
+            to="/login"
+            order={{ base: 2, slg: 3 }}
+            bg="applegreen"
+            alignItems="center"
+            height="auto"
+            color="white"
+            px={4}
+            py={3}
+            _hover={{ bg: 'applegreen', color: 'black' }}
+            fontFamily="new"
+            display={{ base: 'block', xl: 'none' }}
+          >
+            Login
+          </Button>
+        )}
 
         {/* Nav Links */}
         <Flex align="center" className="nav-menu" mx={10} alignItems="center" justifyContent="center" display={{ base: 'none', slg: 'flex' }}>
@@ -92,21 +95,24 @@ const Navbar = () => {
             <DropdownItem href="/about">About Us</DropdownItem>
             <DropdownItem href="/appointment">Make an Appointment</DropdownItem>
           </DropdownMenu>
-          <Button
-            as={reactrouterlink}
-            to="/login"
-            bg="applegreen"
-            alignItems="center"
-            height="auto"
-            color="white"
-            px={4}
-            py={3}
-            _hover={{ bg: 'applegreen', color: 'black' }}
-            fontFamily="new"
-            display={{ base: 'none', xl: 'block' }}
-          >
-            Login
-          </Button>
+          {!token && (
+            <Button
+              as={reactrouterlink}
+              to="/login"
+              bg="applegreen"
+              alignItems="center"
+              height="auto"
+              color="white"
+              px={4}
+              py={3}
+              _hover={{ bg: 'applegreen', color: 'black' }}
+              fontFamily="new"
+              display={{ base: 'none', xl: 'block' }}
+            >
+              Login
+            </Button>
+          )}
+
         </Flex>
 
         <Flex display={{ base: 'flex', slg: 'none' }} order={{ base: 3 }}>
