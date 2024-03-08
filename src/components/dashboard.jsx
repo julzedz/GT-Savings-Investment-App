a8cce7dfce50c812d126db0716cdc5ed41fdf704
julzedz <edoziechukwuma@gmail.com>
2024-03-08 21:18:08 +0100

    Update transaction dates in Dashboard component

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -77,13 +77,13 @@ const Dashboard = () => {
 
   const transactions = user && user.id === 3 ? [
     {
-      icon: RiUpload2Line, action: 'Withdraw USDT', amount: '-1000.00', date: '2024-03-02 21:37:31', status: 'Completed',
+      icon: RiUpload2Line, action: 'Withdraw USDT', amount: '-1000.00', date: '2024-03-07 21:11:54', status: 'Completed',
+    },
+    {
+      icon: RiUpload2Line, action: 'Withdraw USDT', amount: '-1000.00', date: '2024-03-06 21:37:31', status: 'Completed',
     },
     {
       icon: RiUpload2Line, action: 'Withdraw USDT', amount: '-1000.00', date: '2024-03-02 08:59:31', status: 'Completed',
-    },
-    {
-      icon: RiUpload2Line, action: 'Withdraw USDT', amount: '-1,250.43', date: '2024-02-11 19:42:31', status: 'Completed',
     },
   ] : [];
 
